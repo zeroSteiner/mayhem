83f117040cb1eb769eaf1c1654e6bf75283fc62f
Spencer McIntyre <zeroSteiner@gmail.com>
2014-06-07 14:32:45 -0400

    Improve win_syscall tool to support symbols from JSON

--- tools/win_syscall.py
+++ tools/win_syscall.py
@@ -30,8 +30,10 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+import argparse
 import code
 import ctypes
+import json
 import platform
 import sys
 
@@ -49,17 +51,48 @@ syscall_stub  = syscall_stub.decode('hex')
 
 syscall_prototype = ctypes.CFUNCTYPE(ctypes.c_ulong)
 
+class WindowsSyscall(object):
+	def __init__(self, syscall_map=None):
+		self.syscall_map = syscall_map
+
+		process_h = WindowsProcess(pid=-1)
+		shellcode_sz = align_up(len(syscall_stub), 1024)
+		self.address = process_h.allocate(size=shellcode_sz)
+		process_h.write_memory(self.address, syscall_stub)
+		process_h.protect(self.address, size=shellcode_sz, permissions='PAGE_EXECUTE_READ')
+		self._syscall = syscall_prototype(self.address)
+
+	def __call__(self, syscall_number, *args):
+		return self._syscall(syscall_number, *args)
+
+	def __getattr__(self, syscall_name):
+		if not self.syscall_map:
+			raise AttributeError('no syscall map is available')
+		if not syscall_name in self.syscall_map:
+			raise AttributeError("unknown syscall name: '{0}'".format(syscall_name))
+		syscall_number = self.syscall_map[syscall_name]
+		return lambda *args: self._syscall(syscall_number, *args)
+
 def main():
+	parser = argparse.ArgumentParser(description='win_syscall: Windows native system call utility', conflict_handler='resolve')
+	parser.add_argument('-f', '--file', dest='syscall_file', action='store', type=argparse.FileType('r'), help='json file to load syscall data from')
+	parser.add_argument('-o', '--os', dest='os_name', action='store', help='the windows version to load syscall data for')
+	arguments = parser.parse_args()
+
 	if platform.system() != 'Windows':
 		print("[-] Incompatible platform '{0}'".format(platform.system()))
 		return
-	process_h = WindowsProcess(pid=-1)
-	shellcode_sz = align_up(len(syscall_stub), 1024)
-	address = process_h.allocate(size=shellcode_sz)
-	print("[+] Allocated {0} bytes at 0x{1:08x}".format(shellcode_sz, address))
-	process_h.write_memory(address, syscall_stub)
-	process_h.protect(address, size=shellcode_sz, permissions='PAGE_EXECUTE_READ')
-	syscall = syscall_prototype(address)
+
+	syscall_map = None
+	if arguments.syscall_file and arguments.os_name:
+		syscall_map = json.load(arguments.syscall_file)
+		if not arguments.os_name in syscall_map:
+			print("[-] Invalid os name '{0}'".format(arguments.os_name))
+			return
+		syscall_map = [arguments.os_name]
+		print("[+] Loaded {0} syscall symbols".format(len(syscall_map)))
+	syscall = WindowsSyscall(syscall_map)
+	print("[+] Allocated syscall stub at 0x{0:08x}".format(syscall.address))
 
 	console = code.InteractiveConsole(dict(syscall=syscall))
 	console.interact()
