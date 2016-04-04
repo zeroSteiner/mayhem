2d1950cf505a5fe189b87140da8b074056ffff0e
Spencer McIntyre <zeroSteiner@gmail.com>
2016-04-04 16:48:06 -0400

    Move the windows syscall obj to the exploit module

--- tools/win_syscall.py
+++ tools/win_syscall.py
@@ -31,6 +31,7 @@
 #
 
 import argparse
+import binascii
 import code
 import ctypes
 import json
@@ -39,40 +40,7 @@ import platform
 import sys
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from mayhem.proc.windows import WindowsProcess
-from mayhem.utilities import align_up
-
-syscall_stub = '8b442404'		# mov    eax,DWORD PTR [esp+0x4]
-syscall_stub += '83c408'		# add    esp,0x8
-syscall_stub += 'ba0003fe7f'	# mov    edx,0x7ffe0300
-syscall_stub += 'ff12'			# call   DWORD PTR [edx]
-syscall_stub += '83ec08'		# sub    esp,0x8
-syscall_stub += 'c3'			# ret
-syscall_stub = syscall_stub.decode('hex')
-
-syscall_prototype = ctypes.CFUNCTYPE(ctypes.c_ulong)
-
-class WindowsSyscall(object):
-	def __init__(self, syscall_map=None):
-		self.syscall_map = syscall_map
-
-		process_h = WindowsProcess(pid=-1)
-		shellcode_sz = align_up(len(syscall_stub), 1024)
-		self.address = process_h.allocate(size=shellcode_sz)
-		process_h.write_memory(self.address, syscall_stub)
-		process_h.protect(self.address, size=shellcode_sz, permissions='PAGE_EXECUTE_READ')
-		self._syscall = syscall_prototype(self.address)
-
-	def __call__(self, syscall_number, *args):
-		return self._syscall(syscall_number, *args)
-
-	def __getattr__(self, syscall_name):
-		if not self.syscall_map:
-			raise AttributeError('no syscall map is available')
-		if not syscall_name in self.syscall_map:
-			raise AttributeError("unknown syscall name: '{0}'".format(syscall_name))
-		syscall_number = self.syscall_map[syscall_name]
-		return lambda *args: self._syscall(syscall_number, *args)
+from mayhem.exploits.windows import WindowsX86Syscall
 
 def main():
 	parser = argparse.ArgumentParser(description='win_syscall: Windows native system call utility', conflict_handler='resolve')
@@ -92,7 +60,7 @@ def main():
 			return
 		syscall_map = [arguments.os_name]
 		print("[+] Loaded {0} syscall symbols".format(len(syscall_map)))
-	syscall = WindowsSyscall(syscall_map)
+	syscall = WindowsX86Syscall(syscall_map)
 	print("[+] Allocated syscall stub at 0x{0:08x}".format(syscall.address))
 
 	console = code.InteractiveConsole(dict(syscall=syscall))
